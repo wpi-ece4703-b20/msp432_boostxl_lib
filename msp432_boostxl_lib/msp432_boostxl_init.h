205168c16d7949ff9c62271d63198e6a364b6dd9
Patrick Schaumont <patrickschaumont@users.noreply.github.com>
2021-02-08 11:10:39 -0500

    Update msp432_boostxl_init.h

--- msp432_boostxl_lib/msp432_boostxl_init.h
+++ msp432_boostxl_lib/msp432_boostxl_init.h
@@ -1,3 +1,21 @@
+/* msp432_boostxl
+   Written by Patrick Schaumont (pschaumont@wpi.edu) at Worcester Polytechnic Institute in 2020
+
+msp432_boostxl is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3, or (at your option) any later
+version.
+
+msp432_boostxl is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with msp432_boostxl; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+
 #ifndef MSP432_BOOSTXL_INIT_H_
 #define MSP432_BOOSTXL_INIT_H_
 
